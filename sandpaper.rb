6cdd48a9b55098eff39d2c1801239e157dbca188
nluciano233 <nluciano233@gmail.com>
2021-01-27 17:00:24 +0100

    Moved testing code

--- sandpaper.rb
+++ sandpaper.rb
@@ -1,13 +1,8 @@
 require 'dotenv/load'
 require 'discordrb'
-sl = false
 
 sleeping = []
-
-
 bot = Discordrb::Bot.new token: ENV['TOKEN']
-
-
 
 bot.message() do |event|
     if event.message.content.casecmp('sandpaper') == 0
@@ -34,8 +29,6 @@ bot.message(content: '/sandpaper-id') do |event|
   end
 end
 
-
-
 bot.message() do |event|
   if event.author.role?('803707745017659434') && event.author.id != 765140200438366209
     if sleeping.index(event.author.username) == nil
@@ -54,39 +47,5 @@ bot.message() do |event|
   end
 end
 
-=begin
-#TESTING FOR PER-USER TIMER FOR LAVATRICE
-bot.message() do |event|
-  if event.author.role?('803707745017659434')
-    #event.respond "Working. Added to sleep?"
-      if sleeping.index(event.author.username)
-        bot.send_message(803178831330148413, "Already in. Sleeping list: #{sleeping.join(', ')}")
-      else #for the elsif just put the condition == nil
-        sleeping.push(event.author.username)
-        bot.send_message(803178831330148413, "Added. Sleeping list: #{sleeping.join(', ')}")
-        sleep 5
-        sleeping.delete(event.author.username)
-        bot.send_message(803178831330148413, "Time's up, deleted. Sleeping list: #{sleeping.join(', ')}")
-    end
-  end
-end
-=end
-
-=begin TEST TO SEE HOW RUBY TIMER WORKS
-bot.message() do |event|
-  event.respond "Working..."
-  if event.author.role?('803707745017659434') && sl == false 
-    event.respond "It worked, will work again in 5 seconds."
-    sl = true
-    sleep 5.minutes
-    sl = false
-  end
-end
-=end
-
-#how to make the 5 minute timer work only for specific users?
-#for example if user1 triggers is, the function still has to work
-#for user2 if he types in chat.
-
 bot.run
-#bot.send_message(803178831330148413, 'Bot is now active!')
+
